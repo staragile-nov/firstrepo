beffe19afdfb93052248bbae9ab38e7b50835b24
gvsiva2008 <gvsiva2008@gmail.com>
2023-10-29 21:41:40 +0530

    latest version

--- hello.java
+++ hello.java
@@ -2,12 +2,8 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello, siva!"); 
-=======
         System.out.println("Hello, World!");
         System.out.println("Hello, Staragile!");
-        
->>>>>>> f2b06070383eb135900e60b2586b9aecd346c250
     }
 }
