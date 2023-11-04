a14c8a71fd78bcd034b9db50b2035e9d0795d269
gvsiva2008 <gvsiva2008@gmail.com>
2023-11-04 19:41:50 +0530

    line removed from hello java file

--- hello.java
+++ hello.java
@@ -5,6 +5,5 @@ class HelloWorld {
         System.out.println("Hello, siva!"); 
         System.out.println("Hello, World!");
         System.out.println("Hello, Staragile!");
-		System.out.println("Hello, team!");
     }
 }
